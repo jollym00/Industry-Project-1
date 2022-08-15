71823471a01635ff0f69b3cf1f9550f5e87a9ca7
jollym00 <83397882+jollym00@users.noreply.github.com>
2022-08-15 13:06:36 +1000

    Added backend and Frontend
    In this commit I have added the backend and front end for the search page, edit account page and search results page

--- User Portal/php/updateaccount.php
+++ User Portal/php/updateaccount.php
@@ -3,11 +3,13 @@
         include('connection.php');
         session_start();
         $id = $_SESSION['ID'];
-        $email = $_POST['Email'];  
+        $name = $_POST['FullName'];
+        $email = $_POST['email'];  
+        $MobilePhone = $_POST['MobilePhone'];  
         $password = $_POST['Password'];  
         $company = $_POST['company'];  
 
-        $sql = "update customer set Email = '$email', Password = '$password', Company = '$company' WHERE ClinetID=$id";  
+        $sql = "update customer set Email = '$email', Password = '$password', Company = '$company', `Full Name`='$name', MobilePhone =$MobilePhone  WHERE ClinetID=$id";  
                 
         if ($con->query($sql) === TRUE) {
           echo "Record updated successfully";
--- User Portal/search_result.php
+++ User Portal/search_result.php
@@ -4,70 +4,92 @@
 <head>
 	<title>Legislation Search Result</title>
 	<meta charset="UTF-8">
-	<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /> -->
-
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+	<link href="css/main.css" rel="stylesheet">
 </head>
 
 <body>
-	<nav>
-		<a href="homepage.html"><button type="button" name="HomeBtn" id="HomeBtn">Home</button></a>
-		<a href="account.php"><button type="button" name="AccountBtn" id="AccountBtn">Account</button></a>
-		<a href="login.php"> <button type="button" name="LoginBtn" id="LoginBtn">Logout</button></a>
-	</nav>
-    <?php
-	include('php/connection.php'); 
-    session_start();
-    $id = $_SESSION['ID'];
-    $search = $_POST['search'];
-    $_SESSION['search'] = $search;
-    $sql = "SELECT * FROM search where LegNum=$search  ;" ;
-    if($result = mysqli_query($con, $sql)){
-        if(mysqli_num_rows($result) > 0){
-            echo "<table>";
-                echo "<tr>";
-                    echo "<th>Act</th>";
-                    echo "<th>Division</th>";
-                    echo "<th>Legislation Num</th>";
-                    echo "<th>Legislation Name</th>";
-                    echo "<th>Content</th>";
-                    echo "<th>Anitech Reccomendation</th>";
-                    echo "<th>Subscription Status</th>";
-                echo "</tr>";
-            while($row = mysqli_fetch_array($result)){
-                echo "<tr>";
-				
-                    echo "<td>" . $row['Act'] . "</td>";
-                    echo "<td>" . $row['Division'] . "</td>";
-                    echo "<td>" . $row['LegNum'] . "</td>";
-                    echo "<td>" . $row['LegName'] . "</td>";
-                    echo "<td>" . $row['Content'] . "</td>";
-                    echo "<td>" . $row['AniRec'] . "</td>";
-                    if($row['ClinetID'] != $id) { echo "<td>Unubscribed</td>";}
-                      else { echo "<td>Subscribed</td>"; } 
-					if($row['ClinetID'] != $id) {echo "<td> <form method='post' action='php/Subscribe.php'>
-                        <input type='number' name='clientID' value=$id hidden>
-                        <input type='number' name='LawID' value=" . $row['legislationID']. " hidden>
-                        <input type='submit' value='Subscribe'> 
-                        </form> <td>";}
-                      else { echo "<td> <form method='post' action='php/Unsubscribe.php'>
-                        <input type='number' name='clientID' value=$id hidden>
-                        <input type='number' name='LawID' value=" . $row['legislationID']. " hidden>
-                        <input type='submit' value='Unsubscribe'> 
-                        </form> <td>";}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+	<nav class="navbar sticky-top navbar-expand-lg">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="/User Portal/index.html">Lahebo</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			  <span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			  <div class="navbar-nav">
+				<div class="d-flex justify-content-between">
+					<a class="nav-link" href="homepage.html" >Legislation Search</a>
+					<a class="nav-link" href="SubcribeUnsubcribe.php" >My Legislations</a>
+					<a class="nav-link" href="EditAccount.php" >Account</a>
+					<a class="nav-link" href="login.php">Sign Out</a>
+				</div>
+			  </div>
+			</div>
+		</div>
+	</nav> 
+	<br>
+	<br>
+    
+        <?php
+         
+            include('php/connection.php'); 
+            echo "<div class='d-flex justify-content-center'>";
+        session_start();
+        $id = 1;
+        $search = 45;
+        $sql = "SELECT * FROM search where LegNum=$search  ;" ;
+        if($result = mysqli_query($con, $sql)){
+            if(mysqli_num_rows($result) > 0){
+                echo "<table id='searchresults' class='table table-striped'>";
+                    echo "<tr>";
+                        echo "<th>Act</th>";
+                        echo "<th>Division</th>";
+                        echo "<th>Legislation Num</th>";
+                        echo "<th>Legislation Name</th>";
+                        echo "<th>Content</th>";
+                        echo "<th>Anitech Reccomendation</th>";
+                        echo "<th>Subscription Status</th>";
+                        echo "<th> </th>";
+                    echo "</tr>";
+                while($row = mysqli_fetch_array($result)){
+                    echo "<tr>";
                     
-                echo "</tr>";
+                        echo "<td>" . $row['Act'] . "</td>";
+                        echo "<td>" . $row['Division'] . "</td>";
+                        echo "<td>" . $row['LegNum'] . "</td>";
+                        echo "<td>" . $row['LegName'] . "</td>";
+                        echo "<td>" . $row['Content'] . "</td>";
+                        echo "<td>" . $row['AniRec'] . "</td>";
+                        if($row['ClinetID'] != $id) { echo "<td>Unubscribed</td>";}
+                        else { echo "<td>Subscribed</td>"; } 
+                        if($row['ClinetID'] != $id) {echo "<td> <form method='post' action='php/Subscribe.php'>
+                            <input type='number' name='clientID' value=$id hidden>
+                            <input type='number' name='LawID' value=" . $row['legislationID']. " hidden>
+                            <input type='submit' value='Subscribe'> 
+                            </form> <td>";}
+                        else { echo "<td> <form method='post' action='php/Unsubscribe.php'>
+                            <input type='number' name='clientID' value=$id hidden>
+                            <input type='number' name='LawID' value=" . $row['legislationID']. " hidden>
+                            <input type='submit' value='Unsubscribe'> 
+                            </form> <td>";}
+                        
+                    echo "</tr>";
+                }
+                echo "</table>";
+                mysqli_free_result($result);
+            } 
+            else{
+                echo "No records matching your query were found.";
             }
-            echo "</table>";
-            mysqli_free_result($result);
-        } 
-        else{
-            echo "No records matching your query were found.";
         }
-    }
 
 
-    mysqli_close($con);
-?>
+        mysqli_close($con);
+        echo "</div>";
+        ?>
+    
 
 </body>
 </html>

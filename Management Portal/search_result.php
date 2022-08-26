f73982d565c7bdf14c3496a2fa0a846f620cc51e
OS001 <mosethvindi@gmail.com>
2022-08-26 16:49:34 +1000

    front end - legislation search and results

---
+++ Management Portal/search_result.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Legislation Search Result</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+	<link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+	<nav class="navbar sticky-top navbar-expand-lg">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="/User Portal/index.html">Lahebo</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			  <span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			  <div class="navbar-nav">
+				<div class="d-flex justify-content-between">
+					<a class="nav-link" href="search.html" >Legislation Search</a>
+					<a class="nav-link" href="SubcribeUnsubcribe.php" >My Legislations</a>
+					<a class="nav-link" href="EditAccount.php" >Account</a>
+					<a class="nav-link" href="login.php">Sign Out</a>
+				</div>
+			  </div>
+			</div>
+		</div>
+	</nav> 
+	<br>
+	<br>
+
+	<!-- php code -->
+
+</body>
+</html>

c23834d5de96bc09c36a838aba5aceec85740d5d
OS001 <mosethvindi@gmail.com>
2022-08-26 11:42:05 +1000

    front-end homepage and login page, backend - login and authentication

---
+++ Management Portal/php/authentication.php
@@ -0,0 +1,20 @@
+<?php
+	require_once 'PHPGangsta/GoogleAuthenticator.php';
+	include('connection.php');
+	session_start();
+	$id = $_SESSION['ID'];
+    //database table for management users
+	$sql = "SELECT `GoogleCode` FROM `managementUser` where UserID = '$id'";  
+	$a = mysqli_query($con, $sql);  
+	$row = mysqli_fetch_array($a, MYSQLI_ASSOC);
+	$secret = $row["GoogleCode"];
+	$code = $_POST['google_code'];
+	$ga = new PHPGangsta_GoogleAuthenticator();
+	$result = $ga->verifyCode($secret, $code, 3);
+	if ($result == 1){
+		header("Location: ../search.html");
+	}
+	else {
+		echo "TRY again";
+	}
+?>
---
+++ Management Portal/php/login.php
@@ -0,0 +1,26 @@
+<?php 	
+        include('connection.php');
+        session_start();
+          
+        $username = $_POST['Email'];  
+        $password = $_POST['Password'];  
+
+        $username = stripcslashes($username);
+        $password = stripcslashes($password);
+
+        $username = mysqli_real_escape_string($con, $username);
+        $password = mysqli_real_escape_string($con, $password); 
+
+        $sql = "select `UserID` from managementUser where Email = '$username' and Password = '$password'";  
+        $result = mysqli_query($con, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+                
+        if($count == 1){
+          $_SESSION['ID'] = $row["UserID"];
+          header("Location: ../authentication.php");
+        }
+        else{
+            header("Location: ../login.php");
+        }
+?>  

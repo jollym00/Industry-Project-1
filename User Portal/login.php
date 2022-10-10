2d4d1cb82c087884230e0ffcd0adaffa18102c52
jollym00 <83397882+jollym00@users.noreply.github.com>
2022-10-10 13:57:09 +1100

    Payment Updation
    The payment updating has be completed

--- User Portal/login.php
+++ User Portal/login.php
@@ -13,22 +13,23 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 	<nav class="navbar sticky-top navbar-expand-lg ">
 		<div class="container-fluid">
-		<a class="navbar-brand" href="index.html">Lahebo</a>
+			<a class="navbar-brand" href="index.html">Lahebo</a>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 			  <span class="navbar-toggler-icon"></span>
 			</button>
-			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="collapse navbar-collapse " id="navbarNavAltMarkup">
 			  <div class="navbar-nav">
-			  <div class="d-flex justify-content-between">
-					<a class="nav-link" href="#" >Pricing</a>
-					<a class="nav-link" href="#">FAQ</a>
+				<div class="d-flex justify-content-between">
+					<a class="nav-link" href="../Pricing.html" >Pricing</a>
+					<a class="nav-link" href="../FAQ.html">FAQ</a>
 					<a class="nav-link" href="login.php">Login</a>
 					<a class="nav-link" href="sign_up.html">Sign Up</a>
+					<a class="nav-link" href="../ManagementPortal/stafflogin.html" id="stafflogin">Staff Login</a>   
 				</div>
 			  </div>
 			</div>
 		  </div>
-	  </nav> 
+	  </nav>
     <br><br>
     <div class="d-flex justify-content-center">
         

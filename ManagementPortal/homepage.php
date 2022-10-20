a70b6ba539b6fede11d49cfb65c098dd04f47ead
bluelephant72 <89231204+bluelephant72@users.noreply.github.com>
2022-10-20 17:01:23 +1100

    homepage fix

--- ManagementPortal/homepage.php
+++ ManagementPortal/homepage.php
@@ -98,8 +98,7 @@
     
 	<div class="parent container d-flex justify-content-center align-items-center ">
 		<div class="row d-flex justify-content-center">
-			<h1>Welcome to Lahebo Management Portal</h1>
-			<p><a href="login.html">Login</a> or <a href="stafflogin.html">Sign Up</a> Now</p>
+			<h1>Welcome to the Management Portal</h1>
 		</div>
 	
 	</div>

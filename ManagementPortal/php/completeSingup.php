65090cafcb9569ef07a0073396e9426aaaafc7d7
jollym00 <83397882+jollym00@users.noreply.github.com>
2022-10-24 12:22:54 +1100

    update management portal

--- ManagementPortal/php/completeSingup.php
+++ ManagementPortal/php/completeSingup.php
@@ -2,23 +2,28 @@
 <html lang="en">
 
 <head>
-	<title>Payment</title>
+	<title>Welcome to Lahebo</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-	<link href="../css/main.css" rel="stylesheet">
+	<link href="../../css/main.css" rel="stylesheet">
 	<style>	#stafflogin {position:absolute;	right: 10px;}</style>
 </head>
 
-<body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+<body class="pgBody">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 	<?php 	
+	if(empty($_SESSION['role'])){
+		header("Location: ../stafflogin.html");
+	}
     session_start();
     if( $_SESSION['role'] == "superadmin"){
         ?> 
         <nav class="navbar sticky-top navbar-expand-lg ">
 		<div class="container-fluid">
-			<a class="navbar-brand" href="homepage.php">Lahebo</a>
+		<a class="navbar-brand" href="../homepage.php">
+      			<img src="../../img/mainLogo.png" alt="..." height="36">
+    		</a>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 			  <span class="navbar-toggler-icon"></span>
 			</button>
@@ -30,7 +35,7 @@
 					<a class="nav-link" href="../customer.php">Customer</a>
 					<a class="nav-link" href="../sign_up.html">Add Staff</a>
                     <a class="nav-link" href="../Editstaff.php">Staff</a>  
-					<a class="nav-link" href="../stafflogin.html" id="stafflogin">Logout</a>   
+					<a class="nav-link" href="../logout.php" id="stafflogin">Logout</a>   
 				</div>
 			  </div>
 			</div>
@@ -42,15 +47,14 @@
         ?> 
         <nav class="navbar sticky-top navbar-expand-lg ">
 		<div class="container-fluid">
-			<a class="navbar-brand" href="index.html">Lahebo</a>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-			  <span class="navbar-toggler-icon"></span>
-			</button>
+		<a class="navbar-brand" href="../homepage.php">
+      			<img src="../../img/mainLogo.png" alt="..." height="36">
+    		</a>
 			<div class="collapse navbar-collapse " id="navbarNavAltMarkup">
 			  <div class="navbar-nav">
 				<div class="d-flex justify-content-between">
 					<a class="nav-link" href="../customer.php">Customer</a>
-					<a class="nav-link" href="../stafflogin.html" id="stafflogin">Logout</a>   
+					<a class="nav-link" href="../logout.php" id="stafflogin">Logout</a>   
 				</div>
 			  </div>
 			</div>
@@ -65,47 +69,32 @@
         ?> 
         <nav class="navbar sticky-top navbar-expand-lg ">
 		<div class="container-fluid">
-		<a class="navbar-brand" href="#">
-      			<img src="../img/mainLogo.png" alt="..." height="36">
+		<a class="navbar-brand" href="../homepage.php">
+      			<img src="../../img/mainLogo.png" alt="..." height="36">
     		</a>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-			  <span class="navbar-toggler-icon"></span>
-			</button>
 			<div class="collapse navbar-collapse " id="navbarNavAltMarkup">
 			  <div class="navbar-nav">
 				<div class="d-flex justify-content-between">
-					<a class="nav-link" href="../search.html" >Search</a>
-					<a class="nav-link" href=".,/Addlegislation.html">Add Legislation</a>
-                    <a class="nav-link" href=".,/stafflogin.html" id="stafflogin">Logout</a>  
+					<a class="nav-link" href="../search.php" >Search</a>
+					<a class="nav-link" href="../Addlegislation.php">Add Legislation</a>
+                    <a class="nav-link" href="../logout.php" id="stafflogin">Logout</a>  
 				</div>
 			  </div>
 			</div>
 		  </div>
 	  </nav>
-	  
+
 
 <?php
     }
-
-
-
-
-    ?> 
-
-		
-<br>
-		<br>
-	<div>
-		<?php
-		include('connection.php');  
+	include('connection.php');  
 		$txtName = $_POST['name'];
 		$txtEmail = $_POST['email'];
 		$txtPassword = $_POST['password'];
 		$txtPhone = $_POST['phone'];
 		$role = $_POST['role'];
-		$verification = $_POST['verification'];
 
-		if (($role == "superadmin" && $verification == 112233) || ($role == "finance" && $verification == 445566) || ($role == "consultant" && $verification == 778899)){
+		
 			$sql = "INSERT INTO `managementUser`(`Full_Name`, `Email`, `Password`, `Phone`, `role`, `Active`, `GoogleCode`)
 							VALUES ('$txtName', '$txtEmail', '$txtPassword', '$txtPhone', '$role', 'Active', 'null' )";
 							
@@ -120,14 +109,14 @@
 			include('RegisterAuthentication.php'); 
 			echo "</div>";
 			echo "</div>";
-		
-		}
-		else {
-				echo "<p>Account cannot be created due to invalid verification number <a href='../sign_up.html'> go back</a></p>";
-		}
 
-		?>
-	</div>
+
+
+
+?>     
 
 </body>
 </html>
+
+
+
--- ManagementPortal/search_result.php
+++ ManagementPortal/search_result.php
@@ -14,6 +14,9 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 	<?php 	
     session_start();
+    if(empty($_SESSION['role'])){
+      header("Location: ../stafflogin.html");
+    }
     if( $_SESSION['role'] == "superadmin"){
         ?> 
         <nav class="navbar sticky-top navbar-expand-lg ">
